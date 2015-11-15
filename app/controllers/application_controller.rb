c2789bcc914d36b32c8d4779d4e4c3f37ec5f616
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-15 12:30:02 -0500

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render: 'hello world!'
+    render 'layouts/application'
   end
 
 end
